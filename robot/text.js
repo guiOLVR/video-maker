8958854657fd64afea2c24c4729edf4f5c672e5f
Guilherme Monteiro de Oliveira <gui_monteirooliveira@hotmail.com>
2019-03-20 16:46:13 -0300

    Text Robot
    Integration with IBM IA(Watson), and return keywords

--- robot/text.js
+++ robot/text.js
@@ -2,10 +2,23 @@ const algorithmia = require('algorithmia');
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
 const sbd = require('sbd');//sentence boundary detection
 
+const watsonApiKey = require('../credentials/watson.json').apiKey;
+ 
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ 
+var nlu = new NaturalLanguageUnderstandingV1({
+	iam_apikey: "y6i6wbnMJwBovDtYa2GrwTiQXwGJBwE-IgJLAlm_VefZ",
+  	version: '2018-04-05',
+  	url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+
 async function robot(content){
 	await fetchContentFromWikipedia(content);
 	sanitizeContent(content);
 	breakContentIntoSentences(content);
+	limitMaximumSentences(content);
+	await fetchKeywordsOfAllSentences(content);
  
 	async function fetchContentFromWikipedia(content) {
 	    	const algorithmiaAuthenticated = algorithmia('simNamEs9PVGryBRDxPCrDZzlM/1');//autentica o usuario
@@ -57,4 +70,34 @@ async function robot(content){
 		})
 	}
 }
+
+function limitMaximumSentences(content){
+	content.sentences = content.sentences.slice(0, content.maximumSentences);
+}
+
+async function fetchKeywordsOfAllSentences(content){
+	for(const sentence of content.sentences){
+		sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+	}
+}
+
+async function fetchWatsonAndReturnKeywords(sentence){
+	return new Promise((resolve, reject) => {
+		nlu.analyze({
+			text: sentence,
+			features:{
+				keywords: {}
+			}
+		}, (error, response) => {
+			if(error){
+				throw error;
+			}
+			const keywords = response.keywords.map((keyword) => {
+          		return keyword.text
+        	})
+        	resolve (keywords);
+		});
+	})
+}
+
 module.exports = robot;
